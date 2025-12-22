cbceda5a30e828d189402ad835771485b68744f0
ZKZ-Brain <131148083+ZKZ-Brain@users.noreply.github.com>
2025-12-22 22:10:31 +0800

    Update FC.m

--- FC.m
+++ FC.m
@@ -1,5 +1,7 @@
 %computing the functional connectivity network (AAL)
-for i=1:1642
+load('/Users/xyazkz/Downloads/data/depression_FC(848vs794)/Pow_patient_idx.mat');
+number = length(patient_idx);
+for i=1:number
     load(['/Users/xyazkz/Downloads/data/depression_FC(848vs794)/AAL/subj_' num2str(i,'%04d') '_ROI.mat']);
     R=corr(ROI_TC,'type','Pearson');
     R=(R+R')/2;
@@ -12,8 +14,6 @@ for i=1:1642
 end
 
 %computing the functional connectivity network (Power 264 functional ROIs)
-load('/Users/xyazkz/Downloads/data/depression_FC(848vs794)/Pow_patient_idx.mat');
-number = length(patient_idx);
 for i=1:number
     load(['/Users/xyazkz/Downloads/data/depression_FC(848vs794)/Power 264 functional ROIs/subj_' num2str(patient_idx(i),'%04d') '_ROI.mat']);
     R=corr(ROI_TC,'type','Pearson');
